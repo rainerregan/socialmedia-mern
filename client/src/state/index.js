80b2e6dddc3168e7e0e86132649e74edd3c195d6
Rainer Regan <rainercoc@gmail.com>
2023-01-26 15:34:25 +0700

    Redux and Redux Toolkit Installation & Setup

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,6 +1,51 @@
 const { createSlice } = require("@reduxjs/toolkit");
 
-// Global state that accessible through the app
+// Create global states that can be accessible within the app globally
 const initialState = {
-  
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
 }
+
+// Basically reducer is like a function to modify state
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if(state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("User friends non-exsitent");
+      }
+    }, 
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      // Return the new list of post containing the updated ones
+      const updatedPosts = state.posts.map((post) => {
+        if(post._id === action.payload.post_id) return action.payload.post;
+        return post;
+      });
+
+      // Change the list
+      state.posts = updatedPosts;
+    }
+  }
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
+export default authSlice.reducer;
