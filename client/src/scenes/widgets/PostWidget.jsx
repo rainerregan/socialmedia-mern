3640bd76eed1f4582c8e846a5c493c3215761b75
Rainer Regan <rainercoc@gmail.com>
2023-01-27 20:04:14 +0700

    Create Friend widget

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,7 @@
+import { ChatBubbleOutlineOutlined, FavoriteBorderOutlined, FavoriteOutlined, ShareOutlined } from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+
